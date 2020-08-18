3fd5a314c5b936667fc6055b715d287e92cbac95
Deji Adegbite <d2alphame@yahoo.com>
2020-08-18 17:41:20 +0100

    Complete version 1 of the loader

--- loader.asm
+++ loader.asm
@@ -1,13 +1,26 @@
 ; This is the program loader of e6. It is responsible for loading e6 binaries
 ; into memory and executing them. The program must contain a header tag within
-; its first 65536 byte (64kb). The entirety of the header tag must be within
-; this range and must be 8-byte aligned. Note that all e6 programs are loaded at
-; memory address 0x200000.
+; its first 65536 bytes (64kb). The header tag must be 8-byte aligned. Note that
+; all e6 programs are loaded at memory address 0x200000.
 ; The following is the header tag which e6 searches for
 ;
 ; VALUE			SIZE	NAME			DESCRIPTION
 ;=========================================================================================
 ; 'FOR E6OS'	8		Signature		The signature string aligned on 8 byte boundary
 ; EntryPoint	8		Entry Point		Offset into the image to start executing code from
-; Reserved		16		Reserved		Reserved for future use
+; Reserved		16		Reserved		Must be zeros. Reserved for future use
 
+
+mov rax, 'FOR E6OS'							; The signature value to find
+mov rdi, 0x200000							; Start address to begin search from
+mov rcx, 0x2000								; This is the number of string quads to search
+
+begin:
+repnz scasq									; Search for the e6 signature
+jnz .not_found								; If zero flag is set here then the signature was not found
+
+; Prepare to get the entry point
+mov rsi, rdi
+lodsq										; Gets the entry point in rax
+mov rbx, rax								; Move it over to rbx
+call rbx									; Make the call to the program
