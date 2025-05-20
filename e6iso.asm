fa4d46ce5e9852cf3fb0d044da25d18f7a94e67d
Deji Adegbite <d2alphame@yahoo.com>
2025-05-20 21:33:12 +0100

    Add e6iso.asm

---
+++ e6iso.asm
@@ -0,0 +1,6 @@
+; Licensed under GPLv3. See the LICENSE file
+; Assemble with 
+;   nasm -f bin -o e6.iso e6iso.asm
+; This assembles into a 700MiB iso 9660 bootable CD image
+; Boot up a virtual machine with the resulting image or burn it to a disk and 
+; boot a real machine with it.
