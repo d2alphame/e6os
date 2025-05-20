7619b3e7ef7bc99bd99f1ca8c15597cfae0825bc
Deji Adegbite <d2alphame@yahoo.com>
2025-05-20 22:26:38 +0100

    Add source file e6installer.asm

---
+++ e6installer.asm
@@ -0,0 +1,23 @@
+; Copyright (C) 2025 Deji Adegbite
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;
+; See the LICENSE file
+
+; Assemble with
+;     nasm -f bin -o e6installer.bin e6installer.asm
+; This is the first part of the installation program of e6. The assembled binary
+; should be no more than 32KiB and should go to the systems area of the 
+; installation CD/iso image
+
