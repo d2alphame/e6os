a3f77d8986844eed586e38f3cc92f2730b90b1bd
Deji Adegbite <d2alphame@yahoo.com>
2025-05-21 11:08:19 +0100

    Add e6constants.asm

--- e6installer.asm
+++ e6installer.asm
@@ -21,12 +21,127 @@
 ; should be no more than 32KiB and should go to the systems area of the 
 ; installation CD/iso image
 
+%include "e6constants.asm"
+
 START:
-mov ah, 0x0E
-mov bx, 0x0007
-mov al, 'E'
-int 10h
+
+cli                           ; Clear interrupts
+
+; Setup a stack we can work with. With the following setup, we keep our fingers
+; crossed and hope we don't overwrite the IVT and the BDA :D
+xor ax, ax
+mov ss, ax
+mov sp, 0x8000
+mov bp, sp
+
+
+; Setup a flat memory segment with CS=DS=ES
+mov ax, cs
+mov ds, ax
+mov es, ax
+
+sti                           ; Set interrupts again
+
+mov ax, 0x7c0
+mov ds, ax
+mov es, ax
+xor eax, eax
+push cs
+pop ax
+call print_eax_hex
 jmp $
+
+; mov si, STARTUP_MSG
+; xor al, al
+; call print_byte_terminated_string
+; jmp $
+
+; mov ah, 0x0E
+; mov bx, 0x0007
+; mov al, 'E'
+; int 10h
+; jmp $
+
+
+
+; A bunch of functions that will be useful
+; ========================================
+
+
+print_byte_terminated_string:
+  ; Prints a string that is terminated by a given byte
+  ; IN:
+  ;    DS:SI - Location of string to print
+  ;    AL    - The byte that terminates the string
+  ;----------------------------------------------------
+
+; mov ah, 0x0E
+; mov bx, 0x0007
+; mov al, 'E'
+; int 10h
+; jmp $
+
+  push dx
+  mov dl, al
+  mov ah, 0x0E
+  mov bx, 0x0007
+  .loop:
+    lodsb
+    cmp al, dl
+    je .done
+    int 10h
+    jmp .loop
+  .done:
+    pop dx
+    ret
+
+print_eax_hex:
+  ; Prints out the content of the eax register in hexadecimal
+  ; IN:
+  ;    EAX - The value to print
+  ; ---------------------------------------------------------
+
+  mov edx, eax                                ; Preserve the eax value in edx
+  mov di, EAX_HEX.hexstring
+  mov cx, 0x08                                ; Number of nibbles in a double word
+  mov bx, HEX_DIGITS
+  .loop:
+    rol edx, 0x04
+    mov eax, edx
+    and eax, 0x0F
+    xlatb
+    stosb
+    loop .loop
+
+    ; Print the hexadecimal representation
+    mov bx, 0x0007
+    mov ah, 0x0E
+    mov si, EAX_HEX
+    mov cx, 0x0A
+    .fetch:
+      lodsb
+      int 10h
+      loop .fetch
+    ret
+
+
+clear_screen:
+  ; Clears the screen
+ 
+
+wait_for_key:
+  ; Waits for a given key to be pressed
+
+STARTUP_MSG:  
+  db "E6OS INSTALLATION CD", 0x0A, 0x0D, 0x0A, 0x0D
+  db "If you're seeing this, it means your system boots from BIOS.", 0x0A, 0x0D
+  db "Press the enter key to continue with the installation of e6os.", 0x00
+
+EAX_HEX:
+  .prefix: db "0x"
+  .hexstring: dq 0x00
+
+HEX_DIGITS: db "0123456789ABCDEF", 0x00
 
 times 32768-($-START) db 0
 
--- e6iso.asm
+++ e6iso.asm
@@ -36,6 +36,8 @@
 ; ??                              Path Table (MSB)
 ; ??                              Root Directory
 
+%include "e6constants.asm"
+
 START_ISO:
 incbin 'e6installer.bin'
 times 32768-($-START_ISO) db 0                    ; ISO 9660 specifies that the 32kb is reserved and may be used for other stuff
@@ -173,7 +175,7 @@ BOOT_CATALOG:
     INITIAL_DEFAULT_ENTRY:
         .boot_indicator         db 0x88                                 ; Bootable. 0x00 for not bootable
         .boot_media_type        db 0x00                                 ; No emulation
-        .load_segment           dw 0x800                                ; Segment where the image will be loaded. Make this 0 to default to 0x7C0
+        .load_segment           dw 0x000                                ; Segment where the image will be loaded. Make this 0 to default to 0x7C0
         .system_type            db 0xEE                                 ; Must be a copy of byte 5 from the partition table in the boot image
         .unused_1               db 0x00
         .sector_count           dw 0x10                                 ; Number of sectors to load into memory during boot.
