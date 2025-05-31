49607a10be9e7194718305882dab77119085a434
Deji Adegbite <d2alphame@yahoo.com>
2025-05-31 18:15:02 +0100

    Still trying to print hello world

--- BOOTX64.asm
+++ BOOTX64.asm
@@ -88,7 +88,12 @@ EntryPoint:
     mov [EFI_SYSTEM_TABLE], rdx
     add rdx, EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
     mov rcx, [rdx]
-
+    mov rdx, [rdx]
+    add rdx, EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_OutputString
+    mov rax, rdx
+    lea rdx, [hello_message]
+    sub rsp, 40
+    call rax
 
 
     xor rax, rax
@@ -100,12 +105,12 @@ DATA:
     EFI_IMAGE_HANDLE    dq 0x00                                             ; EFI will give us this in rcx
     EFI_SYSTEM_TABLE    dq 0x00                                             ; And this in rdx
     
-    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL                 equ 64
-    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_OutputString    equ 8
-    
     hello_message db __utf16__ `Hello_world\0`                              ; EFI strings are UTF16 and null-terminated
 
 
 align 4096
 HEADER_END:
 END:
+
+EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL                 equ 64
+EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_OutputString    equ 8
