ffa5e170e958ba18c72b9c841a790c27e6e41c76
Deji Adegbite <d2alphame@yahoo.com>
2026-04-21 19:00:37 +0100

    Minor edits

--- BOOTX64.asm
+++ BOOTX64.asm
@@ -45,22 +45,19 @@ OPTIONAL_HEADER_START:
     .SECTION_ALIGNMENT:          dd 0x0004                       ; Alignment in bytes of sections when they are loaded in memory. Align to page boundry (4kb)
     .FILE_ALIGNMENT:             dd 0x1000                       ; Alignment of sections in the file. Also align to 4kb
 
-    ; What would normally follow should be MAJOR_OS_VERSION (2 bytes), MINOR_OS_VERSION (2 bytes), MAJOR_IMAGE_VERSION (2 bytes), MINOR_IMAGE_VERSION (2 bytes).
-    ; These make a total of 8 bytes. E6 would use that 8 bytes to store the EFI_IMAGE_HANDLE
-    ; .EFI_IMAGE_HANDLE:             dq 0x00                         ; This will be passed to us by EFI when e6 boots
+    ; What would normally follow should be MAJOR_OS_VERSION (2 bytes), MINOR_OS_VERSION (2 bytes), MAJOR_IMAGE_VERSION (2 bytes), MINOR_IMAGE_VERSION (2 bytes),
+    ; MAJOR_SUBSYSTEM_VERSION (2 bytes), MINOR_SUBSYSTEM_VERSION (2 bytes), and WIN32_VERSION_VALUE (4 bytes). This gives a total of 16 bytes. This will be used
+    ; to hold the boot message instead.
+    .BOOT_MESSAGE:                 db __utf16__ `E6OS\r\n\0`       ; EFI strings are UTF16 and null-terminated
+        .boot_msg_padding          dw 0                            ; Pad up the boot message above with 2 bytes it make 16 bytes
 
-    .MAJOR_OS_VERSION:           dw 0x00                         ; I'm not sure UEFI requires these and the following 'version woo'
-    .MINOR_OS_VERSION:           dw 0x00                         ; More of these version thingies are to follow. Again, not sure UEFI needs them
-    .MAJOR_IMAGE_VERSION:        dw 0x00                         ; Major version of the image
-    .MINOR_IMAGE_VERSION:        dw 0x00                         ; Minor version of the image
-    
-    ; MAJOR_SUBSYSTEM_VERSION (2 bytes), MINOR_SUBSYSTEM_VERSION (2 bytes), and WIN32_VERSION_VALUE (4 bytes) would normally follow for a total of 8 bytes. E6 uses
-    ; this to store the pointer to the EFI_SYSTEM_TABLE that would be passed by EFI on boot.
-    ; .EFI_SYSTEM_TABLE:             dq 0x00                         ; Received from EFI on boot
-
-    .MAJOR_SUBSYSTEM_VERSION:    dw 0x00                         ; 
-    .MINOR_SUBSYSTEM_VERSION:    dw 0x00                         ;
-    .WIN32_VERSION_VALUE:        dd 0x00                         ; Reserved, must be 0
+    ; .MAJOR_OS_VERSION:           dw 0x00                         ; I'm not sure UEFI requires these and the following 'version woo'
+    ; .MINOR_OS_VERSION:           dw 0x00                         ; More of these version thingies are to follow. Again, not sure UEFI needs them
+    ; .MAJOR_IMAGE_VERSION:        dw 0x00                         ; Major version of the image
+    ; .MINOR_IMAGE_VERSION:        dw 0x00                         ; Minor version of the image
+    ; .MAJOR_SUBSYSTEM_VERSION:    dw 0x00                         ; 
+    ; .MINOR_SUBSYSTEM_VERSION:    dw 0x00                         ;
+    ; .WIN32_VERSION_VALUE:        dd 0x00                         ; Reserved, must be 0
     
     .IMAGE_SIZE:                 dd END - START                  ; The size in bytes of the image when loaded in memory including all headers
     .HEADERS_SIZE:               dd HEADER_END - HEADER_START    ; Size of all the headers
@@ -105,25 +102,18 @@ EntryPoint:
 
     add rdx, EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_OutputString
     mov rax, [rdx]
-    lea rdx, [hello_message]
+    lea rdx, [OPTIONAL_HEADER_START.BOOT_MESSAGE]    ; The boot message. We're going to print it.
 
-    sub rsp, 40
+    sub rsp, 40                ; Make room on the stack along with the shadow space
     call rax
 
     add rsp, 40
-    xor rax, rax
+    mov rax EFI_SUCCESS        ; UEFI use rax = 0 for success
     jmp $
     ret
 
 
-align 16
-DATA:
-    ; EFI_IMAGE_HANDLE    dq 0x00                                            ; EFI will give us this in rcx
-    ; EFI_SYSTEM_TABLE    dq 0x00                                            ; And this in rdx
-    hello_message db __utf16__ `Hello World\r\n\0`                         ; EFI strings are UTF16 and null-terminated
-
-
-times 4096 - ($ - START) db 0x00                      ; Pad up to 4kb
+;times 4096 - ($ - START) db 0x00                      ; Pad up to 4kb
 HEADER_END:
 END:
 
