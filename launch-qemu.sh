91a8ee21a007f18a35483d870ceef11f9bcdeaf3
Deji Adegbite <d2alphame@yahoo.com>
2026-05-26 23:38:25 +0100

    Update .gitignore and add launch-qemu script

---
+++ launch-qemu.sh
@@ -0,0 +1,12 @@
+qemu-system-x86_64 \
+  -machine type=q35,accel=tcg \
+  -cpu qemu64 \
+  -smp 2 \
+  -m 2048 \
+  -drive if=pflash,format=raw,readonly=on,file=/usr/local/share/qemu/edk2-x86_64-code.fd \
+  -drive if=pflash,format=raw,file=./nvram-vars.fd \
+  -drive if=none,id=usbdisk,format=raw,file=usb-flash.img \
+  -device usb-ehci,id=ehci \
+  -device usb-storage,bus=ehci.0,drive=usbdisk \
+  -usb \
+  -boot menu=on
